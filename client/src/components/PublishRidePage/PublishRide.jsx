3e100f4a2afb965620c8a43927708a396da2431b
Nikhil <nikhil.4002.50.82@gmail.com>
2024-12-03 23:20:24 +0530

    saved-changes

--- client/src/components/PublishRidePage/PublishRide.jsx
+++ client/src/components/PublishRidePage/PublishRide.jsx
@@ -21,16 +21,16 @@ const PublishRide = () => {
   return (
     <div className='p-[8em] h-[auto] flex flex-col'>
         <div className='flex justify-center items-center'>
-          <h1 className='text-4xl font-semibold p-4'>Pick-up</h1>
+          <h1 className='text-4xl text-blue-950 font-semibold p-4'>Pick-up</h1>
         </div>
         <div className='p-3 flex justify-center items-center'>
-          <input className='w-[70%] h-[2.7em] rounded-xl p-3 bg-[#EDEDED]' type="text" placeholder='Enter the full address' name='driverPickUpLoc' value={pickUp} onChange={(event)=>{
+          <input className='w-[65%] focus:outline-[#00AFF5] h-[3em] rounded-xl p-3 bg-[#EDEDED]' type="text" placeholder='Enter the full address' name='driverPickUpLoc' value={pickUp} onChange={(event)=>{
                         let value=event.target.value;
                         setPickUp(value);
                         AutoComplete(value);
                     }}  />
                         {res && res.length > 0 && (
-                            <div className="absolute w-[75%] p-3 top-[16em] left-[10em] right-0 mt-1 bg-white  border border-gray-300 rounded-xl shadow-lg max-h-60 overflow-y-auto z-10" >
+                            <div className="absolute w-[75%] p-3 top-[16.5em] left-[10em] right-0 mt-1 bg-white  border border-gray-300 rounded-xl shadow-lg max-h-60 overflow-y-auto z-10" >
                                 {res.map((item,index) => (
                                     <div key={index} className="p-4 pt-0 flex flex-col items-center justify-center hover:bg-[#EDEDED] cursor-pointer rounded-lg">
                                         <h1 className="flex items-center justify-center h-[3em] text-lg ">{item.display_place}</h1>
