32d597f19f1dfccb57ae5ee83876593102409a5f
Nikhil <nikhil.4002.50.82@gmail.com>
2024-12-03 23:03:03 +0530

    updated home-img

--- client/src/components/HomePage/TitleHome.jsx
+++ client/src/components/HomePage/TitleHome.jsx
@@ -6,14 +6,14 @@ import { IoCarSport } from "react-icons/io5";
 
 const TitleHome = () => {
   return (
-        <div className='h-[18em] bg-[#00AFF5] p-5 pt-1 pb-1 flex flex-col mb-4'>
-            <h1 className='text-white font-semibold text-6xl text-center'>Carpool to thousands of destinations at low prices</h1>
-            <div className='flex items-center justify-center'><IoCarSport className='text-9xl text-gray-100' /></div>
-            <div className='top-[0.7em]'>
+        <div className='h-[16em] bg-[url("bg.svg")] bg-center bg-cover p-5 pt-1 pb-1 flex flex-col justify-center mb-4'>
+            <h1 className='text-white font-bold text-5xl text-center relative top-[-1em]'>Carpool to thousands of destinations at low prices</h1>
+            {/* <div className='flex items-center justify-center'><IoCarSport className='text-9xl text-gray-100' /></div> */}
+            <div className='relative top-[7em]'>
                 <TitleHomeComponent />
             </div>        
         </div>    
   );
-};
+}
 
 export default TitleHome;
